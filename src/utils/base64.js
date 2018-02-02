22d2707127270e1fd0b07868cdb7f4e39777698d
Scott Taylor <scott.c.taylor@mac.com>
2018-02-02 15:41:36 -0500

    add tests to utils

--- src/utils/base64.js
+++ src/utils/base64.js
@@ -1,12 +1,15 @@
+// Client notes:
 // https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
+// The "Unicode Problem"
 
-// first we use encodeURIComponent to get percent-encoded UTF-8,
-// then we convert the percent encodings into raw bytes which
-// can be fed into btoa.
-export function base64EncodeUnicode(str) {
+function base64EncodeUnicode(str) {
+  // Server uses Buffer API
   if (typeof document === 'undefined') {
     return Buffer.from(str).toString('base64');
   }
+  // In most browsers, calling btoa() on a Unicode string will cause a Character Out Of Range exception.
+  // First we use encodeURIComponent to get percent-encoded UTF-8,
+  // then we convert the percent encodings into raw bytes which can be fed into btoa.
   return btoa(
     encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (match, p1) =>
       String.fromCharCode(`0x${p1}`)
@@ -14,11 +17,12 @@ export function base64EncodeUnicode(str) {
   );
 }
 
-// Going backwards: from bytestream, to percent-encoding, to original string.
-export function base64DecodeUnicode(str) {
+function base64DecodeUnicode(str) {
+  // Server uses Buffer API
   if (typeof document === 'undefined') {
     return Buffer.from(str, 'base64').toString();
   }
+  // Going backwards: from bytestream, to percent-encoding, to original string.
   return decodeURIComponent(
     atob(str)
       .split('')
@@ -26,3 +30,5 @@ export function base64DecodeUnicode(str) {
       .join('')
   );
 }
+
+export { base64EncodeUnicode as base64Encode, base64DecodeUnicode as base64Decode };
--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,16 +1,23 @@
 // @flow
-import { base64DecodeUnicode, base64EncodeUnicode } from 'utils/base64';
+import { base64Decode, base64Encode } from 'utils/base64';
 
 const PREFIX = 'arrayconnection:';
 
 export function cursorToOffset(cursor: string) {
-  return parseInt(base64DecodeUnicode(cursor).substring(PREFIX.length), 10);
+  return parseInt(base64Decode(cursor).substring(PREFIX.length), 10);
 }
 
 export function offsetToCursor(offset: number) {
-  return base64EncodeUnicode(PREFIX + offset);
+  return base64Encode(PREFIX + offset);
 }
 
+/**
+ * All instances of infinite scroll follow the same convention
+ * - Relay-based pagination using the Connection spec
+ * - Always loading more items after the last loaded item
+ *
+ * To avoid component inheritance, `this` must be bound when calling this function
+ */
 export function bindLoadMore(prop: string) {
   return (e: Event): Promise<{}> => {
     e.preventDefault();
