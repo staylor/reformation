5da499b5622a8db067b835ae5c9196fcbb901978
Scott Taylor <scott.c.taylor@mac.com>
2020-12-12 16:45:26 -0500

    use hooks for Modals

--- src/components/Modals/Media.js
+++ src/components/Modals/Media.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import { graphql } from '@apollo/client/react/hoc';
-import { gql } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
+import debounce from 'lodash.debounce';
 import Loading from 'components/Loading';
 import { uploadUrl } from 'utils/media';
 import {
@@ -15,155 +15,144 @@ import {
 
 /* eslint-disable react/prop-types */
 
-@graphql(
-  gql`
-    query MediaModalQuery($type: String, $first: Int, $cursor: String) {
-      uploads(after: $cursor, first: $first, type: $type) @connection(key: "media") {
-        edges {
-          node {
-            id
-            title
-            type
-            destination
-            fileName
-            ... on ImageUpload {
-              crops {
-                width
-                fileName
+function MediaModal({ type, onClose, selectAudio, selectImage }) {
+  const frameRef = useRef(null);
+  const { loading, fetchMore, data } = useQuery(
+    gql`
+      query MediaModalQuery($type: String, $first: Int, $cursor: String) {
+        uploads(after: $cursor, first: $first, type: $type) @connection(key: "media") {
+          edges {
+            node {
+              id
+              title
+              type
+              destination
+              fileName
+              ... on ImageUpload {
+                crops {
+                  width
+                  fileName
+                }
               }
-            }
-            ... on AudioUpload {
-              images {
-                width
-                fileName
+              ... on AudioUpload {
+                images {
+                  width
+                  fileName
+                }
               }
             }
           }
-        }
-        pageInfo {
-          hasNextPage
-          endCursor
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
         }
       }
+    `,
+    {
+      variables: { first: 50, type },
+      fetchPolicy: 'network-only',
     }
-  `,
-  {
-    options: ({ type }) => {
-      const variables = { first: 25 };
+  );
 
-      if (type) {
-        variables.type = type;
-      }
-
-      return {
-        variables,
-        fetchPolicy: 'cache-and-network',
-      };
-    },
-  }
-)
-class MediaModal extends Component {
-  loadMore = () => {
-    const { fetchMore, variables, uploads } = this.props.data;
-    return fetchMore({
-      variables: {
-        ...variables,
-        cursor: uploads.pageInfo.endCursor,
-      },
-    });
-  };
-
-  frameHandler = frame => {
-    if (!frame) {
+  const scrollListener = debounce(() => {
+    const { uploads } = data;
+    const hasNext = uploads.pageInfo.hasNextPage;
+    if (!hasNext || loading) {
       return;
     }
-    frame.addEventListener('scroll', () => {
-      const { uploads, loading } = this.props.data;
-      const hasNext = uploads.pageInfo.hasNextPage;
-      if (!hasNext || loading) {
-        return;
-      }
-      if (frame.scrollTop + frame.offsetHeight >= frame.scrollHeight) {
-        this.loadMore();
-      }
-    });
-  };
+    if (
+      frameRef.current.scrollTop + frameRef.current.offsetHeight >=
+      frameRef.current.scrollHeight
+    ) {
+      fetchMore({
+        variables: {
+          cursor: uploads.pageInfo.endCursor,
+        },
+      });
+    }
+  }, 500);
 
-  modalRef = null;
-
-  render() {
-    const {
-      data: { loading, uploads },
-    } = this.props;
-
-    const portal = document.getElementById('portal');
-
-    if (loading && !uploads) {
-      return ReactDOM.createPortal(
-        <div className={modalClass}>
-          <Loading />
-        </div>,
-        portal
-      );
+  useEffect(() => {
+    const frame = frameRef.current;
+    if (data) {
+      frame.addEventListener('scroll', scrollListener);
     }
 
+    return () => {
+      if (data) {
+        frame.removeEventListener('scroll', scrollListener);
+      }
+    };
+  }, [data, scrollListener]);
+
+  const portal = document.getElementById('portal');
+
+  if (!loading && !data) {
+    return null;
+  }
+
+  if (loading && !data) {
     return ReactDOM.createPortal(
-      <div
-        className={modalClass}
-        innerRef={ref => {
-          this.modalRef = ref;
-        }}
-        id="media-modal"
-      >
-        <CloseButton className="dashicons dashicons-no" onClick={this.props.onClose} />
-        <div className={frameClass} ref={this.frameHandler}>
-          {uploads.edges.map(({ node }) => {
-            const prop = this.type === 'audio' ? 'images' : 'crops';
-            const crop = node[prop] && node[prop].find(c => c.width === 150);
-            return (
-              <Item
-                key={node.id}
-                onClick={e => {
-                  e.preventDefault();
-
-                  if (node.type === 'audio') {
-                    this.props.selectAudio(node);
-                  } else {
-                    const normalized = {
-                      destination: node.destination,
-                      crops: [],
-                    };
-
-                    node.crops.forEach(({ width, fileName }) => {
-                      normalized.crops.push({ width, fileName });
-                    });
-
-                    this.props.selectImage({
-                      imageId: node.id,
-                      image: normalized,
-                      size: 'FEATURE',
-                    });
-                  }
-
-                  this.props.onClose(e);
-                }}
-              >
-                {crop ? (
-                  <img
-                    className={itemImageClass}
-                    alt=""
-                    src={uploadUrl(node.destination, crop.fileName)}
-                  />
-                ) : null}
-                <span className={itemTitleClass}>{node.title}</span>
-              </Item>
-            );
-          })}
-        </div>
+      <div className={modalClass}>
+        <Loading />
       </div>,
       portal
     );
   }
+
+  const { uploads } = data;
+
+  return ReactDOM.createPortal(
+    <div className={modalClass} id="media-modal">
+      <CloseButton className="dashicons dashicons-no" onClick={onClose} />
+      <div className={frameClass} ref={frameRef}>
+        {uploads.edges.map(({ node }) => {
+          const prop = type === 'audio' ? 'images' : 'crops';
+          const crop = node[prop] && node[prop].find(c => c.width === 150);
+          return (
+            <Item
+              key={node.id}
+              onClick={e => {
+                e.preventDefault();
+
+                if (node.type === 'audio') {
+                  selectAudio(node);
+                } else {
+                  const normalized = {
+                    destination: node.destination,
+                    crops: [],
+                  };
+
+                  node.crops.forEach(({ width, fileName }) => {
+                    normalized.crops.push({ width, fileName });
+                  });
+
+                  selectImage({
+                    imageId: node.id,
+                    image: normalized,
+                    size: 'FEATURE',
+                  });
+                }
+
+                onClose(e);
+              }}
+            >
+              {crop ? (
+                <img
+                  className={itemImageClass}
+                  alt=""
+                  src={uploadUrl(node.destination, crop.fileName)}
+                />
+              ) : null}
+              <span className={itemTitleClass}>{node.title}</span>
+            </Item>
+          );
+        })}
+      </div>
+    </div>,
+    portal
+  );
 }
 
 export default MediaModal;
--- src/components/Modals/Video.js
+++ src/components/Modals/Video.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import { graphql } from '@apollo/client/react/hoc';
-import { gql } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
+import debounce from 'lodash.debounce';
 import Loading from 'components/Loading';
 import Video from 'components/Videos/Video';
 import {
@@ -15,113 +15,120 @@ import {
 
 /* eslint-disable react/prop-types */
 
-@graphql(
-  gql`
-    query VideoModalQuery($cursor: String, $first: Int) {
-      videos(after: $cursor, first: $first) @connection(key: "videos") {
-        edges {
-          node {
-            id
-            ...Video_video
+function VideoModal({ selectVideo, onClose }) {
+  const frameRef = useRef(null);
+  const { loading, fetchMore, data } = useQuery(
+    gql`
+      query VideoModalQuery($cursor: String, $first: Int) {
+        videos(after: $cursor, first: $first) {
+          edges {
+            node {
+              id
+              ...Video_video
+            }
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
           }
         }
-        pageInfo {
-          hasNextPage
-          endCursor
-        }
       }
-    }
-    ${Video.fragments.video}
-  `,
-  {
-    options: {
-      variables: { first: 50 },
-    },
-  }
-)
-class VideoModal extends Component {
-  loadMore = () => {
-    const { fetchMore, variables, videos } = this.props.data;
-    return fetchMore({
+      ${Video.fragments.video},
+    `,
+    {
       variables: {
-        ...variables,
-        first: 25,
-        cursor: videos.pageInfo.endCursor,
+        first: 50,
       },
-    });
-  };
+    }
+  );
 
-  frameHandler = frame => {
-    if (!frame) {
+  const scrollListener = debounce(() => {
+    const { videos } = data;
+    const hasNext = videos.pageInfo.hasNextPage;
+    if (!hasNext || loading) {
       return;
     }
-    frame.addEventListener('scroll', () => {
-      const hasNext = this.props.data.videos.pageInfo.hasNextPage;
-      if (!hasNext || this.props.data.loading) {
-        return;
-      }
-      if (frame.scrollTop + frame.offsetHeight >= frame.scrollHeight) {
-        this.loadMore();
-      }
-    });
-  };
+    if (
+      frameRef.current.scrollTop + frameRef.current.offsetHeight >=
+      frameRef.current.scrollHeight
+    ) {
+      fetchMore({
+        variables: {
+          cursor: videos.pageInfo.endCursor,
+        },
+      });
+    }
+  }, 500);
 
-  render() {
-    const {
-      data: { loading, videos },
-    } = this.props;
-
-    const portal = document.getElementById('portal');
-
-    if (loading && !videos) {
-      return ReactDOM.createPortal(
-        <div className={modalClass}>
-          <Loading />
-        </div>,
-        portal
-      );
+  useEffect(() => {
+    const frame = frameRef.current;
+    if (data) {
+      frame.addEventListener('scroll', scrollListener);
     }
 
+    return () => {
+      if (data) {
+        frame.removeEventListener('scroll', scrollListener);
+      }
+    };
+  }, [data, scrollListener]);
+
+  const portal = document.getElementById('portal');
+
+  if (!loading && !data) {
+    return null;
+  }
+
+  if (loading && !data) {
     return ReactDOM.createPortal(
       <div className={modalClass}>
-        <CloseButton className="dashicons dashicons-no" onClick={this.props.onClose} />
-        <div className={frameClass} ref={this.frameHandler}>
-          {videos.edges.map(({ node }) => {
-            const crop = node.thumbnails.find(c => c.width === 120);
-            return (
-              <div // eslint-disable-line
-                className={videoItemClass}
-                key={node.id}
-                onClick={e => {
-                  e.preventDefault();
-
-                  const normalized = {
-                    dataId: node.dataId,
-                    title: node.title,
-                    slug: node.slug,
-                    thumbnails: [],
-                  };
-                  node.thumbnails.forEach(({ width, height, url }) => {
-                    normalized.thumbnails.push({ width, height, url });
-                  });
-
-                  this.props.selectVideo({
-                    videoId: node.id,
-                    video: normalized,
-                  });
-                  this.props.onClose(e);
-                }}
-              >
-                <img className={itemImageClass} alt="" src={crop.url} />
-                <span className={itemTitleClass}>{node.title}</span>
-              </div>
-            );
-          })}
-        </div>
+        <Loading />
       </div>,
-      document.getElementById('portal')
+      portal
     );
   }
+
+  const { videos } = data;
+
+  return ReactDOM.createPortal(
+    <div className={modalClass}>
+      <CloseButton className="dashicons dashicons-no" onClick={onClose} />
+      <div className={frameClass} ref={frameRef}>
+        {videos.edges.map(({ node }) => {
+          const crop = node.thumbnails.find(c => c.width === 120);
+          return (
+            <div // eslint-disable-line
+              className={videoItemClass}
+              key={node.id}
+              onClick={e => {
+                e.preventDefault();
+
+                const normalized = {
+                  dataId: node.dataId,
+                  title: node.title,
+                  slug: node.slug,
+                  thumbnails: [],
+                };
+                node.thumbnails.forEach(({ width, height, url }) => {
+                  normalized.thumbnails.push({ width, height, url });
+                });
+
+                selectVideo({
+                  videoId: node.id,
+                  video: normalized,
+                });
+                onClose(e);
+              }}
+            >
+              <img className={itemImageClass} alt="" src={crop.url} />
+              <span className={itemTitleClass}>{node.title}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>,
+    document.getElementById('portal')
+  );
 }
 
 export default VideoModal;
