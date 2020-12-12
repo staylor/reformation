3942a9485915fa3755e9a4b91fb7874da5203bda
Scott Taylor <scott.c.taylor@mac.com>
2020-12-12 00:13:12 -0500

    Terms: use refetch query

--- src/routes/Admin/Terms/ListTable.js
+++ src/routes/Admin/Terms/ListTable.js
@@ -7,33 +7,86 @@ import ListTable, { renderThumbnail } from 'components/ListTable';
 import { rowActionsClass, rowTitleClass, thumbnailColumnClass } from 'components/ListTable/styled';
 import { offsetToCursor } from 'utils/connection';
 import { Heading, HeaderAdd } from 'routes/Admin/styled';
-import TermQuery from './TermQuery.graphql';
 
 /* eslint-disable react/prop-types */
 
 const PER_PAGE = 20;
 
 @compose(
-  graphql(TermQuery, {
-    options: ({ match }) => {
-      const { params } = match;
-
-      const variables = { first: PER_PAGE, taxonomyId: params.taxonomyId };
-      if (params.page) {
-        const pageOffset = parseInt(params.page, 10) - 1;
-        if (pageOffset > 0) {
-          variables.after = offsetToCursor(pageOffset * PER_PAGE - 1);
+  graphql(
+    gql`
+      query TermQuery(
+        $first: Int
+        $after: String
+        $taxonomyId: ObjID!
+        $taxonomy: String
+        $search: String
+      ) {
+        terms(
+          first: $first
+          after: $after
+          taxonomyId: $taxonomyId
+          taxonomy: $taxonomy
+          search: $search
+        ) @connection(key: "terms", filter: ["taxonomyId", "taxonomy", "search"]) {
+          taxonomy {
+            id
+            name
+            slug
+            plural
+          }
+          count
+          edges {
+            node {
+              id
+              name
+              slug
+              taxonomy {
+                id
+              }
+              featuredMedia {
+                ... on ImageUpload {
+                  type
+                  destination
+                  crops {
+                    fileName
+                    width
+                  }
+                }
+              }
+              ... on Venue {
+                capacity
+                address
+              }
+            }
+          }
+          pageInfo {
+            hasNextPage
+          }
         }
       }
+    `,
+    {
+      options: ({ match }) => {
+        const { params } = match;
 
-      return {
-        // This ensures that the table is up to date when taxonomies are mutated.
-        // The alternative is to specify refetchQueries on all Taxonomy mutations.
-        variables,
-        fetchPolicy: 'cache-and-network',
-      };
-    },
-  }),
+        const variables = { first: PER_PAGE, taxonomyId: params.taxonomyId };
+        if (params.page) {
+          const pageOffset = parseInt(params.page, 10) - 1;
+          if (pageOffset > 0) {
+            variables.after = offsetToCursor(pageOffset * PER_PAGE - 1);
+          }
+        }
+
+        return {
+          // This ensures that the table is up to date when taxonomies are mutated.
+          // The alternative is to specify refetchQueries on all Taxonomy mutations.
+          variables,
+          fetchPolicy: 'cache-and-network',
+        };
+      },
+    }
+  ),
   graphql(gql`
     mutation DeleteTermMutation($ids: [ObjID]!) {
       removeTerm(ids: $ids)
@@ -46,7 +99,7 @@ class Terms extends Component {
       location,
       match,
       mutate,
-      data: { variables, loading, terms },
+      data: { variables, refetch, loading, terms },
     } = this.props;
 
     if (loading && !terms) {
@@ -75,10 +128,11 @@ class Terms extends Component {
             e.preventDefault();
 
             mutate({
-              refetchQueries: [{ query: TermQuery, variables }],
               variables: {
                 ids: [term.id],
               },
+            }).then(() => {
+              refetch();
             });
           };
 
@@ -123,7 +177,12 @@ class Terms extends Component {
         <Heading>{terms.taxonomy.plural}</Heading>
         <HeaderAdd to={`/terms/${terms.taxonomy.id}/add`}>Add {terms.taxonomy.name}</HeaderAdd>
         <ListTable
-          {...{ location, match, columns, mutate, variables }}
+          location={location}
+          match={match}
+          columns={columns}
+          mutate={mutate}
+          refetch={refetch}
+          variables={variables}
           data={terms}
           path={`/terms/${terms.taxonomy.id}`}
         />
