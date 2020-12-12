a6b7e23263d9e654c3ae9c88f4692aaa262d78ce
Scott Taylor <scott.c.taylor@mac.com>
2020-12-12 00:13:44 -0500

    Shows: use refetch query

--- src/routes/Admin/Shows/ListTable.js
+++ src/routes/Admin/Shows/ListTable.js
@@ -7,7 +7,6 @@ import ListTable from 'components/ListTable';
 import { rowActionsClass, rowTitleClass } from 'components/ListTable/styled';
 import { offsetToCursor } from 'utils/connection';
 import { Heading, HeaderAdd } from 'routes/Admin/styled';
-import ShowQuery from './ShowQuery.graphql';
 
 /* eslint-disable react/prop-types,react/no-multi-comp */
 
@@ -16,15 +15,16 @@ const PER_PAGE = 20;
 const columns = [
   {
     label: 'Title',
-    render: (show, { mutate, variables }) => {
+    render: (show, { mutate, refetch }) => {
       const onClick = e => {
         e.preventDefault();
 
         mutate({
-          refetchQueries: [{ query: ShowQuery, variables }],
           variables: {
             ids: [show.id],
           },
+        }).then(() => {
+          refetch();
         });
       };
 
@@ -59,26 +59,69 @@ const columns = [
 ];
 
 @compose(
-  graphql(ShowQuery, {
-    options: ({ match }) => {
-      const { params } = match;
-
-      const variables = { first: PER_PAGE, order: 'DESC' };
-      if (params.page) {
-        const pageOffset = parseInt(params.page, 10) - 1;
-        if (pageOffset > 0) {
-          variables.after = offsetToCursor(pageOffset * PER_PAGE - 1);
+  graphql(
+    gql`
+      query ShowQuery(
+        $first: Int
+        $after: String
+        $date: Float
+        $taxonomy: String
+        $term: String
+        $search: String
+        $order: ShowOrder
+      ) {
+        shows(
+          first: $first
+          after: $after
+          date: $date
+          taxonomy: $taxonomy
+          term: $term
+          search: $search
+          order: $order
+        ) @connection(key: "shows", filter: ["date", "taxonomy", "term", "search"]) {
+          count
+          edges {
+            node {
+              id
+              title
+              date
+              artist {
+                id
+                name
+              }
+              venue {
+                id
+                name
+              }
+            }
+          }
+          pageInfo {
+            hasNextPage
+          }
         }
       }
+    `,
+    {
+      options: ({ match }) => {
+        const { params } = match;
 
-      return {
-        variables,
-        // This ensures that the table is up to date when shows are mutated.
-        // The alternative is to specify refetchQueries on all Show mutations.
-        fetchPolicy: 'cache-and-network',
-      };
-    },
-  }),
+        const variables = { first: PER_PAGE, order: 'DESC' };
+        if (params.page) {
+          const pageOffset = parseInt(params.page, 10) - 1;
+          if (pageOffset > 0) {
+            variables.after = offsetToCursor(pageOffset * PER_PAGE - 1);
+          }
+        }
+
+        return {
+          variables,
+          // This ensures that the table is up to date when shows are mutated.
+          // The alternative is to specify refetchQueries on all Show mutations.
+          fetchPolicy: 'cache-and-network',
+        };
+      },
+    }
+  ),
   graphql(
     gql`
       mutation DeleteShowMutation($ids: [ObjID]!) {
@@ -93,7 +136,7 @@ class Shows extends Component {
       location,
       match,
       mutate,
-      data: { variables, loading, shows },
+      data: { loading, shows, refetch, variables },
     } = this.props;
 
     if (loading && !shows) {
@@ -104,7 +147,16 @@ class Shows extends Component {
       <>
         <Heading>Show</Heading>
         <HeaderAdd to="/show/add">Add Show</HeaderAdd>
-        <ListTable {...{ location, match, columns, mutate, variables }} data={shows} path="/show" />
+        <ListTable
+          location={location}
+          match={match}
+          columns={columns}
+          mutate={mutate}
+          refetch={refetch}
+          variables={variables}
+          data={shows}
+          path="/show"
+        />
       </>
     );
   }
